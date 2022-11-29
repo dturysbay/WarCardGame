3f3e0e2fe762c917448c5a5cd6b2d63b07b710ca
Dinmukhambet Turysbay <d.turysbay@yandex.kz>
2022-11-30 01:13:27 +0600

    scoring added

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftScoreLabel: UILabel!
     
     @IBOutlet weak var rightScoreLabel: UILabel!
+    var leftScore = 0
+    var rightScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +27,25 @@ class ViewController: UIViewController {
 
 
     @IBAction func dealTapped(_ sender: UIButton) {
-        
+//        Randomize numbers for updating image views
         let leftNumber = Int.random(in: 2...14)
         let rightNumber = Int.random(in: 2...14)
         
+//        Update image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber{
+            
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+            
+        }else if leftNumber < rightNumber {
+            
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+            
+        }
     }
 }
 
